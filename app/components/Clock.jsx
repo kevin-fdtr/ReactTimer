c409f467cb84e1685db2c1469be63a19555cb238
Kevin Sorensen <kevin@fdtr.ca>
2016-05-26 07:22:48 -0600

    formatSeconds Clock test

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,13 +1,18 @@
 var React = require('react');
 
 var Clock = React.createClass({
-    render: function () {
-      return (
-        <div>
+  formatSeconds: function (totalSeconds) {
+    var seconds = totalSeconds % 60;
+    var minutes = Math.florr(totalSeconds / 60);
+    return minutes  + ":" + seconds;
+  },
+  render: function () {
+    return (
+      <div>
 
-        </div>
-      );
-    }
+      </div>
+    );
+  }
 });
 
 module.exports = Clock;
