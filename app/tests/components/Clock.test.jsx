d85c6720b767811053f7f7710234703a59923b9d
Kevin Sorensen <kevin@fdtr.ca>
2016-05-26 22:59:43 -0600

    finished clock tests and styled clock

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -10,11 +10,47 @@ describe('Clock', () => {
   it('should exist', () => {
     expect(Clock).toExist();
   });
+  describe('render', () => {
+    it('should render clock to output', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+      var $el = $(ReactDOM.findDOMNode(clock));
+      var actualText = $el.find('.clock-text').text();
+      expect(actualText).toBe('01:02');
+    });
+  });
   describe('formatSeconds', () => {
     it('should format seconds', () => {
-      var clock = TestUtils.renderIntoDocumemt(<Clock/>);
-      var second = 615;
+      var clock = TestUtils.renderIntoDocument(<Clock/>);
+      var seconds = 615;
       var expected = '10:15';
+      var actual = clock.formatSeconds(seconds);
+      expect(actual).toBe(expected);
+    });
+    it('should format seconds when min/sec are less than 10', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock/>);
+      var seconds = 61;
+      var expected = '01:01';
+      var actual = clock.formatSeconds(seconds);
+      expect(actual).toBe(expected);
+    });
+    it('should format minutes when min are 0 ', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock/>);
+      var seconds = 1;
+      var expected = '00:01';
+      var actual = clock.formatSeconds(seconds);
+      expect(actual).toBe(expected);
+    });
+    it('should format seconds when seconds are 0 ', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock/>);
+      var seconds = 0;
+      var expected = '00:00';
+      var actual = clock.formatSeconds(seconds);
+      expect(actual).toBe(expected);
+    });
+    it('should format minutes when min are > 60', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock/>);
+      var seconds = 6000;
+      var expected = '100:00';
       var actual = clock.formatSeconds(seconds);
       expect(actual).toBe(expected);
     });
